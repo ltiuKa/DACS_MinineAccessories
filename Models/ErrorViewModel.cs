4ce887de00ff96c8432ed59ac7643c7a79069011
Ka đang học Code <anhbao88thang@gmail.com>
2024-04-22 22:45:06 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DACS_MinineAccessories.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
